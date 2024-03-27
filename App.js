e2952fb428aa814b0ff0684a17dcbdc36ae6457c
Esha Chiplunkar <eshac7@gmail.com>
2024-03-27 19:36:09 -0400

    lab 5 section 3.1

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import Hello from "./Hello.js"
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
 const app = express()
 Hello(app)
+Lab5(app)
 app.listen(4000)
