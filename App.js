d819cacae9d16026b36025c1a5db42007d42d75b
Esha Chiplunkar <eshac7@gmail.com>
2024-03-30 10:40:54 -0400

    everything till assignments done

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";
+import ModuleRoutes from './Kanbas/modules/routes.js';
 const app = express()
 app.use(cors());
 app.use(express.json());
 Hello(app)
 Lab5(app)
+CourseRoutes(app);
+ModuleRoutes(app);
 app.listen(4000)
