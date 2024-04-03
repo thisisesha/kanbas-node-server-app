f45d82caec90bac9097e35741290f6f21cca4c81
Esha Chiplunkar <eshac7@gmail.com>
2024-04-03 01:09:46 -0400

    a6 lab

--- App.js
+++ App.js
@@ -5,9 +5,13 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import AssignmentRoutesApp from './Kanbas/assignments/routes.js';
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 Hello(app)
 Lab5(app)
 CourseRoutes(app);
