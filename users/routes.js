19d399c45e01ecd84aba29a98a446c396e4409d8
Esha Chiplunkar <eshac7@gmail.com>
2024-04-04 16:36:30 -0400

    complete till section 5

--- users/routes.js
+++ users/routes.js
@@ -26,7 +26,7 @@ export default function UserRoutes(app) {
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
-    currentUser = await dao.findUserById(userId);
+    //currentUser = await dao.findUserById(userId);
     res.json(status);
   };
   const signup = async (req, res) => {
