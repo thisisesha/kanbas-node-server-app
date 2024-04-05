47411c88f30c991c9d0ab5c345aa7e05bc52fce0
Esha Chiplunkar <eshac7@gmail.com>
2024-04-05 00:51:05 -0400

    courses changes

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -1,44 +1,39 @@
-import Database from "../Database/index.js";
+import * as dao from "./dao.js";
+
 export default function CourseRoutes(app) {
-  app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
-    res.send(courses);
-  });
 
-  app.post("/api/courses", (req, res) => {
-    const course = { ...req.body,
-      _id: new Date().getTime().toString() };
-    Database.courses.push(course);
-    res.send(course);
-  });
+  const findAllCourses = async (req, res) => {
+    const courses = await dao.findAllCourses();
+    res.json(courses);
+  };
 
-  app.delete("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    Database.courses = Database.courses
-      .filter((c) => c._id !== id);
-    res.sendStatus(204);
-  });
+  const createCourse = async (req, res) => {
+    const course = await dao.createCourse(req.body);
+    console.log(course);
+    res.json(course);
+  };
 
-  app.put("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    const course = req.body;
-    Database.courses = Database.courses.map((c) =>
-      c._id === id ? { ...c, ...course } : c
-    );
-    res.sendStatus(204);
-  });
+  const deleteCourse = async (req, res) => {
+    const status = await dao.deleteCourse(req.params.courseId);
+    res.json(status);
+  };
 
-  app.get("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    const course = Database.courses
-      .find((c) => c._id === id);
-    if (!course) {
-      res.status(404).send("Course not found");
-      return;
-    }
-    res.send(course);
-  });
+  const updateCourse = async (req, res) => {
+    const { courseId } = req.params;
+    const status = await dao.updateCourse(courseId, req.body);
+    //currentUser = await dao.findUserById(userId);
+    res.json(status);
+  };
+
+  const findCourseById = async (req, res) => {
+    const course = await dao.findCourseById(req.params.courseId);
+    res.json(course);
+  };
 
 
-
+app.get("/api/courses/:id", findCourseById);
+app.get("/api/courses", findAllCourses);
+app.post("/api/courses", createCourse);
+app.put("/api/courses/:id", updateCourse);
+app.delete("/api/courses/:id", deleteCourse);
 }
---
+++ Kanbas/courses/schema.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const courseSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    number: { type: String, required: true },
+    startDate: Date,
+    endDate: Date,
+    image: String,
+  },
+  { collection: "courses" });
+export default courseSchema;
